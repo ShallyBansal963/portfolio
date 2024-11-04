78d22b1c339912a54deede4539986cb17a5a6ff5
Shelly BBansal <168955377+ShallyBansal963@users.noreply.github.com>
2024-11-04 23:05:25 +0100

    changse

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function toggleMenu() {
     // Select the menu and hamburger icon elements
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+    menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 
     // Check if the elements exist before toggling classes
     if (menu && icon) {
