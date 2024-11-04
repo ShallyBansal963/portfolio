9cb59cf64a77a8ad16a2fcf4dba26264dbdf1758
Shelly BBansal <168955377+ShallyBansal963@users.noreply.github.com>
2024-11-05 00:02:15 +0100

    made it mobile friendly

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+    document.querySelector('.hamburger-icon').addEventListener('click', function() {
+        this.classList.toggle('open');
+        document.querySelector('.menu-links').classList.toggle('open');
+      });
+      
+    
 
     // Check if the elements exist before toggling classes
     if (menu && icon) {
         menu.classList.toggle("open");
         icon.classList.toggle("open");
+        
     } else {
         console.error("Menu or icon element not found");
     }
